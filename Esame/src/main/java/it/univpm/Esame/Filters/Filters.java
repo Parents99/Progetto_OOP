bb5176332c9e838cd193ad39680f0cb4e64d42b5
Parents99 <christianparente6@gmail.com>
2021-12-02 15:58:01 +0100

    filtri finiti e città

--- Esame/src/main/java/it/univpm/Esame/Filters/Filters.java
+++ Esame/src/main/java/it/univpm/Esame/Filters/Filters.java
@@ -4,9 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-
-
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties.Template;
 import org.springframework.stereotype.Service;
 
 import it.univpm.Esame.Exception.BodyException;
@@ -25,17 +25,46 @@ public class Filters {
 		JsonParser downloader = new JsonParser();
 		ArrayList<Lavoro> annunci = downloader.Parsing();
 		
-		//filtro location
-		if (body.getLocation()!=null) {
-			if(body.getLocation() != "") { 
-				for(int i=0;i<annunci.size();i++)
-					if(annunci.get(i).getLuogo().contains(body.getLocation())==false) {
+		
+		//fare filtro location per singola città come metodo e richiamarlo in base a locations.length
+		//otteniamo due liste diverse e le metti insieme
+		
+		
+		
+		
+		// filtro location
+		if (body.getLocation() != null) {
+			if(body.getLocation().contains(" ")) { // caso più città nel filtro
+				ArrayList<String> tmp = new ArrayList<String>();
+				boolean flag;
+				String[] locations=body.getLocation().split(" ");
+				for(int j=0;j<locations.length;j++)
+					tmp.add(locations[j]);
+				
+				for(int i=0;i<annunci.size();i++) {
+					flag = false;
+					for(int h=0;h<tmp.size();h++) {
+						if(annunci.get(i).getLuogo().contains(tmp.get(h)) == true) { //condizione sbagliata?
+							flag = true;
+						}
+					}
+					if (flag == false) {
 						annunci.remove(i);
 						i--;
 					}
-			}
+				}		
+				
+			} else if(body.getLocation() != "") { 
+					for(int i=0;i<annunci.size();i++)
+						if(annunci.get(i).getLuogo().contains(body.getLocation())==false) {
+							annunci.remove(i);
+							i--;
+						}
+					}
 		}
 		
+		
+		//filtro location
 		
 		//filtro orario full-time part-time contratto
 		if (body.getOrario()!=null) {
@@ -63,7 +92,7 @@ public class Filters {
 		
 		//filtro remoto		
 		if (body.getRemoto() != null){
-			if(body.getRemoto() instanceof Boolean == false)
+			if(body.getRemoto() instanceof Boolean == false) //controllo se ha inserito un booleano
 				e.InvalidBody("Remoto è di tipo booleano, inserire true o false");
 			if(body.getRemoto() != false) {
 				for(int i=0;i<annunci.size();i++)
@@ -86,7 +115,10 @@ public class Filters {
 		
 		if(annunci.size()==0)
 			e.BodyResults("Nessun risultato trovato");
+		
 		return annunci;
 			
 	} // fine metodo
+	
+	
 }
