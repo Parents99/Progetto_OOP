c434be0b08ab85becb3897b9c5908b8b61592472
Parents99 <christianparente6@gmail.com>
2021-12-04 16:48:24 +0100

    avanzamento statistiche,manca top 5

--- Esame/src/main/java/it/univpm/Esame/Model/StatResult.java
+++ Esame/src/main/java/it/univpm/Esame/Model/StatResult.java
@@ -4,24 +4,60 @@ import java.util.ArrayList;
 
 public class StatResult {
 	private int numTot; //numero totale di annunci
+	private int numTotLocation; //numero totale di annunci in una località
 	private String fulltimePerc; //percentuale di lavori full time 
 	private int numFulltime; //numero di lavori part time
 	private String partimePerc; //percentuale di lavori
 	private int numPartime;  //numero di lavori part time
+	private String contractPerc; //percentuale di lavori a contratto
+	private int numContract;
 	private ArrayList<String> lan;	 //linguaggi richiesti insieme al javascript
 	private String bestJob;  // ruoli più richiesti
 	
 	public StatResult() {
 		this.numTot=0;
+		this.numTotLocation=0;
 		this.fulltimePerc="";
 		this.numFulltime=0;
 		this.partimePerc="";
 		this.numPartime=0;
+		this.contractPerc="";
+		this.numContract = 0;
 		this.lan=new ArrayList<String>();
 		this.bestJob="";
 	} 
 	
 	
+	public String getContractPerc() {
+		return contractPerc;
+	}
+
+
+	public void setContractPerc(String contractPerc) {
+		this.contractPerc = contractPerc;
+	}
+
+
+	public int getNumContract() {
+		return numContract;
+	}
+
+
+	public void setNumContract() {
+		this.numContract++;
+	}
+
+
+	public int getNumTotLocation() {
+		return numTotLocation;
+	}
+
+
+	public void setNumTotLocation(int numTotLocation) {
+		this.numTotLocation = numTotLocation;
+	}
+
+
 	public void setNumTot(int numTot) {
 		this.numTot = numTot;
 	}
--- Esame/src/main/java/it/univpm/Esame/Statistics/Statistics.java
+++ Esame/src/main/java/it/univpm/Esame/Statistics/Statistics.java
@@ -1,6 +1,7 @@
 package it.univpm.Esame.Statistics;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -16,6 +17,7 @@ import it.univpm.Esame.Service.JsonParser;
 
 @Service
 public class Statistics {
+	
 	public StatResult Statistic(RequestBody body) throws IOException {
 		
 		StatResult risultati = new StatResult();  //valore di ritorno con le statistiche
@@ -23,45 +25,61 @@ public class Statistics {
 		ArrayList<Lavoro> annunci = download.Parsing();  // scarico il json
 		risultati.setNumTot(annunci.size());  //num totale di annunci (per le percentuali)
 		ArrayList<String> tmp = new ArrayList<String>(); // arraylist di keyword
+		int j=0;
 		
-		for(int i=0;i<annunci.size();i++) { //manca i top 5 dei lavori richiesti
-			if(body.getLocation().contains(annunci.get(i).getLuogo())){
-				if(annunci.get(i).getOrario().equalsIgnoreCase("full time"))
-					risultati.setNumFulltime(); //metodi set che incrementano solamente
-				if(annunci.get(i).getOrario().equalsIgnoreCase("part time"))
-					risultati.setNumPartime();  
-				tmp.addAll(annunci.get(i).getKeyword()); //concateno tutti i keyword di ogni annuncio in un unico arraylist
+		if(body.getLocation() !="" && body.getLocation()!=null) {	
+			for(int i=0;i<annunci.size();i++) { //manca i top 5 dei lavori richiesti
+				if(body.getLocation().contains(annunci.get(i).getLuogo())){
+					if(annunci.get(i).getOrario().equalsIgnoreCase("full time"))
+						risultati.setNumFulltime(); //metodi set che incrementano solamente
+					if(annunci.get(i).getOrario().equalsIgnoreCase("part time"))
+						risultati.setNumPartime();  
+					if(annunci.get(i).getOrario().equalsIgnoreCase("contract"))
+						risultati.setNumContract();
+					if(annunci.get(i).getOrario().equals("null")) //annunci senza orari, per contare numLocalità
+						j++;
+					tmp.addAll(annunci.get(i).getKeyword()); //concateno tutti i keyword di ogni annuncio in un unico arraylist
+				}
 			}
-				
-		}
+		}else {
+			for(int i=0;i<annunci.size();i++) { //manca i top 5 dei lavori richiesti
+					if(annunci.get(i).getOrario().equalsIgnoreCase("full time"))
+						risultati.setNumFulltime(); //metodi set che incrementano solamente
+					if(annunci.get(i).getOrario().equalsIgnoreCase("part time"))
+						risultati.setNumPartime();  
+					if(annunci.get(i).getOrario().equalsIgnoreCase("contract"))
+						risultati.setNumContract();
+					if(annunci.get(i).getOrario().equals("null")) //annunci senza orari, per contare numLocalità
+						j++;
+					tmp.addAll(annunci.get(i).getKeyword()); //concateno tutti i keyword di ogni annuncio in un unico arraylist
+				}
+			}
 		
-		Set<String> set = new HashSet<String>(tmp);  //lo metto dentro un hashSet che non consente i duplicati
-		tmp.clear();  //cancello gli elementi dell'arraylist
-		tmp.addAll(set);  //e ci rimetto l'hashset privo di duplicati
+			Set<String> set = new HashSet<String>(tmp);  //lo metto dentro un hashSet che non consente i duplicati
+			tmp.clear();  //cancello gli elementi dell'arraylist
+			tmp.addAll(set);  //e ci rimetto l'hashset privo di duplicati
 		
-		//calcolo percentuale full time
-		int full=0;
-		int totale=risultati.getNumTot();
-		double percentuale=(full/totale)*100;
+			//calcolo percentuale full time
+			risultati.setNumTotLocation(risultati.getNumFulltime()+risultati.getNumPartime()+risultati.getNumContract()+j);
+		
+			double percentuale1 = (risultati.getNumFulltime()/ (double) risultati.getNumTotLocation())*100;
+			risultati.setFulltimePerc(String.format("%.01f", percentuale1)+"%");
+		
+			//calcolo percentuale part time
+			double percentuale2=(risultati.getNumPartime()/ (double) risultati.getNumTotLocation())*100;
+			risultati.setPartimePerc(String.format("%.01f", percentuale2)+"%");
+		
+			double percentuale3 = (risultati.getNumContract()/ (double) risultati.getNumTotLocation())*100;
+			risultati.setContractPerc(String.format("%.01f", percentuale3)+"%");
+	
+			risultati.setLan(tmp);
 		
 		
 		
-		String top= percentuale + "%";
-		risultati.setFulltimePerc(top);
 		
-		//calcolo percentuale part time
-		top=(risultati.getNumPartime()/risultati.getNumTot())*100+"%";
-		risultati.setPartimePerc(top);
-		
-		risultati.setLan(tmp);
 		
 		return risultati;
 	}
+	
 
-	
-	
-	
-	
-	
-	
 }
